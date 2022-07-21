01f4a815e43ef7c926e3d68023ecff34b349cc52
Abdullah Adeel <easyautosol@gmail.com>
2022-07-21 16:39:17 +0500

    redirect user to home after post deletion

--- client/lib/mutaions/posts.ts
+++ client/lib/mutaions/posts.ts
@@ -1,12 +1,13 @@
 import { useToast } from "@chakra-ui/react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/router";
 import { POST_QUERY } from "../../constants/queries";
 import { postApi } from "../../services/postsApi";
 
 export const useDeletePostMutation = () => {
   const toast = useToast();
   const queryClient = useQueryClient();
-
+  const router = useRouter();
   return useMutation(postApi.deletePost, {
     onSuccess: () => {
       toast({
@@ -14,6 +15,7 @@ export const useDeletePostMutation = () => {
         status: "success",
       });
       queryClient.invalidateQueries([POST_QUERY]);
+      router.push("/");
     },
   });
 };
